796a5f4925417a0ea63ae275ea4a5a51764248b9
Nicholas Knapton <nickknapton12@gmail.com>
2021-08-07 13:42:28 -0700

    Updates

---
+++ static/js/53.d922689f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[53],{140:function(t,n,o){"use strict";o.r(n),o.d(n,"startStatusTap",(function(){return r}));var e=o(13),i=o(12),r=function(){var t=window;t.addEventListener("statusTap",(function(){Object(e.h)((function(){var n=t.innerWidth,o=t.innerHeight,r=document.elementFromPoint(n/2,o/2);if(r){var c=r.closest("ion-content");c&&new Promise((function(t){return Object(i.c)(c,t)})).then((function(){Object(e.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.d922689f.chunk.js.map
